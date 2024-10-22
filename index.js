23dd3294906bd07db4834202930c0b1c3366a152
Aadarsh <v16ash@gmail.com>
2024-10-22 18:10:10 +0530

    Add sameple getEvents method
    Exploring the response packages

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
+async function getEvents (username) {
+  // url to set source
+  const url = 'https://api.github.com/users/' + username + '/events';
+  
+  // creating request object
+  let request = new Request(
+    url,
+    {
+      method: 'GET',
+    }
+  );
+  
+  let response = await fetch(request);
+  
+  console.log(response);
+}
+
 // take input username from arguments
 const username = process.argv[2];
+
+getEvents(username);
